714dad73e94af71464f72139cda50a694fa342c4
Brian Matheny <bwmatheny@gmail.com>
2020-05-20 19:16:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,4 @@
+
 def oxford_comma(array)
-  if array.length == 1
-    return array.join
-  elsif array.length == 2
-    return array.join(" and ")
-  else
-    array.last.insert(0, 'and ')
-    array.join(", ")
-  end
+
 end
